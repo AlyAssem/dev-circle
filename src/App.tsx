81cd3044b4ac4104030692436279067bcab26f7c
AlyAssem <alyassem1997@gmail.com>
2021-10-16 17:13:09 +0300

    initialized redux && redux toolkit && redux devtools to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const App: React.FC = () => <div className='aly'>Hello world</div>;
+const App: React.FC = () => <div className='text-green-300'>Hello world</div>;
 
 export default App;
