03b74772f738962ec3532fa45a8d05c47380a6e0
AlyAssem <alyassem1997@gmail.com>
2021-10-16 13:17:56 +0300

    added eslint configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <div>Hello world</div>;
-};
+const App: React.FC = () => <div className='aly'>Hello world</div>;
 
 export default App;
