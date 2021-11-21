683142bf862de498c3bdf18d2998d7e20aa51d20
AlyAssem <alyassem1997@gmail.com>
2021-11-21 16:22:24 +0300

    implemented re-routing to register page for un-registered users

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useEffect } from 'react';
+import { History, LocationState } from 'history';
 
-export const HomePage: React.FC = () => (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-);
+import { Header } from '../../components/Header';
+import { useAppSelector } from '../../redux-features/hooks';
+
+interface IHomePageProps {
+  history: History<LocationState>;
+}
+
+export const HomePage: React.FC<IHomePageProps> = ({
+  history,
+}: IHomePageProps) => {
+  const userInfo = useAppSelector((state) => state.users.userInfo);
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push('/register');
+    }
+  }, [history, userInfo]);
+  return (
+    <div>
+      <Header loggedInUserName={userInfo?.userName} />
+      <h1>Home Page</h1>
+    </div>
+  );
+};
