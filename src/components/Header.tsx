16fafdf184fcbaab039a28236e8945ab9532e74e
AlyAssem <alyassem1997@gmail.com>
2021-11-21 16:20:47 +0300

    implemented responsive navbar

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Menu from '../icons/Menu';
+interface IHeaderProps {
+  loggedInUserName: string | undefined;
+}
+
+export const Header: React.FC<IHeaderProps> = ({
+  loggedInUserName,
+}: IHeaderProps) => {
+  const [isNavigationMenuOpen, setIsNavigationMenuOpen] = useState(false);
+  return (
+    <header>
+      <nav className='h-14 px-5 flex justify-between items-center bg-white shadow'>
+        <div className='flex-grow'>
+          <strong className='bg-green-600 text-white text-lg font-medium leading-5 py-1.5 px-4'>
+            DevCircle
+          </strong>
+        </div>
+        <ul
+          className={`flex justify-end mr-4 text-gray-700 flex-grow transition-transform ${
+            isNavigationMenuOpen ? 'block' : 'hidden'
+          } md:flex`}
+        >
+          <li>
+            <span className='text-green-500 font-medium text-lg mr-4'>
+              {loggedInUserName}
+            </span>
+          </li>
+          <li>
+            <button type='button' onClick={handleLogout}>
+              <span className='text-lg hover:text-green-500'>Logout</span>
+            </button>
+          </li>
+        </ul>
+        <button
+          type='button'
+          className='md:hidden'
+          onClick={() => setIsNavigationMenuOpen((currState) => !currState)}
+        >
+          <Menu />
+        </button>
+      </nav>
+    </header>
+  );
