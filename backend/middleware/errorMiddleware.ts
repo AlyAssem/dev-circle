23a6006a09ed57d2e6524229e47b2d5e5f5f31e0
AlyAssem <alyassem1997@gmail.com>
2022-02-12 01:09:12 +0300

    removed unused code in server after refactor & added custom errorhandl

---
+++ backend/middleware/errorMiddleware.ts
@@ -0,0 +1,16 @@
+const notFound = (req: any, res: any, next: any) => {
+  console.log('NOT FOUND ERROR');
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err: any, req: any, res: any, next: any) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  res.status(statusCode).json({
+    errorMessage: err.message,
+  });
+};
+
+export { notFound, errorHandler };
