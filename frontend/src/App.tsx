43b9cfcf561700dbaec93c9c950a36a807adcaf6
AlyAssem <alyassem1997@gmail.com>
2022-02-15 07:41:16 +0300

    Auto logout when user is idle for 15 mins.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import React, { useEffect } from 'react';
+import { useIdleTimer } from 'react-idle-timer';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { HomePage } from './pages/HomePage/HomePage';
 import { LoginPage } from './pages/LoginPage/LoginPage';
 import { RegisterPage } from './pages/RegisterPage/RegisterPage';
+import { useAppDispatch } from './redux-features/hooks';
+import { logout } from './redux-features/users';
 
-const App: React.FC = () => (
-  <div className='h-screen'>
-    <Router>
-      <Route path='/' component={HomePage} exact />
-      <Route path='/login' component={LoginPage} />
-      <Route path='/register' component={RegisterPage} />
-      <ToastContainer />
-    </Router>
-  </div>
-);
+const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  const handleOnIdle = () => {
+    dispatch(logout());
+  };
+
+  useIdleTimer({
+    timeout: 1000 * 60 * 15,
+    onIdle: handleOnIdle,
+  });
+
+  return (
+    <div className='h-screen'>
+      <Router>
+        <Route path='/' component={HomePage} exact />
+        <Route path='/login' component={LoginPage} />
+        <Route path='/register' component={RegisterPage} />
+        <ToastContainer />
+      </Router>
+    </div>
+  );
+};
 
 export default App;
