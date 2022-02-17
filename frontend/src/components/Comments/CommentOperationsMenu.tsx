7edebdc68b222fc2f7de562d4d1747f19b083dc8
AlyAssem <alyassem1997@gmail.com>
2022-02-17 22:38:07 +0300

    implemented comment operations menu UI for editing, deleting a comment

---
+++ frontend/src/components/Comments/CommentOperationsMenu.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import DropDownIcon from '../../icons/DropDownIcon';
+
+interface ICommentOperationsMenu {
+  onCommentEditPermit: () => void;
+}
+
+const CommentOperationsMenu: React.FC<ICommentOperationsMenu> = ({
+  onCommentEditPermit,
+}) => {
+  const [isCommentOperationsMenuOpen, setIsCommentOperationsMenuOpen] =
+    useState(false);
+
+  return (
+    <div className='relative right-4 inline-block'>
+      <button
+        className='rounded-full focus:ring-2
+      focus:ring-green-400 hover:bg-green-400 hover:bg-opacity-50 '
+        type='button'
+        onClick={(e) => {
+          if (isCommentOperationsMenuOpen) {
+            // unfocus the button when the modal is closed
+            e.currentTarget.blur();
+          }
+          setIsCommentOperationsMenuOpen((prev) => !prev);
+        }}
+      >
+        <DropDownIcon />
+      </button>
+
+      <ul
+        className={`z-10 absolute top-full -left-8 rounded shadow-commentMenu overflow-hidden h-0 ${
+          isCommentOperationsMenuOpen
+            ? 'flex flex-col divide-y-2 divide-green-500 transition-all duration-500 ease-in-out h-fit p-3 bg-white'
+            : ''
+        }`}
+      >
+        <li>
+          <button
+            className='w-full cursor-pointer hover:bg-green-200 focus:bg-green-200 active:bg-green-200'
+            type='button'
+            onClick={() => {
+              onCommentEditPermit();
+            }}
+          >
+            Edit
+          </button>
+        </li>
+        <li className='cursor-pointer hover:bg-green-200'>Delete</li>
+      </ul>
+    </div>
+  );
+};
+
+export default CommentOperationsMenu;
