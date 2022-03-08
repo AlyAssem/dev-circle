5b1f1a71ead773501cd213927c505d99bf854809
AlyAssem <alyassem1997@gmail.com>
2022-03-08 22:46:26 +0300

    fixed error handling override

--- backend/src/middleware/errorMiddleware.ts
+++ backend/src/middleware/errorMiddleware.ts
@@ -1,3 +1,5 @@
+import { Request, Response, NextFunction } from 'express';
+
 const notFound = (req: any, res: any, next: any) => {
   console.log('NOT FOUND ERROR');
   const error = new Error(`Not Found - ${req.originalUrl}`);
@@ -5,7 +7,12 @@ const notFound = (req: any, res: any, next: any) => {
   next(error);
 };
 
-const errorHandler = (err: any, res: any) => {
+const errorHandler = (
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 
   res.status(statusCode).json({
